7a1b7f215ac928735b12a7716a25c53a737ac2c3
oumaimadriya <oumaima-driya@outlook.fr>
2017-05-08 12:53:30 +0100

    add Matreial

--- StockManagement/StockManagement/Presentations/MaterialManger/FormAjouterGroupe.cs
+++ StockManagement/StockManagement/Presentations/MaterialManger/FormAjouterGroupe.cs
@@ -1,4 +1,5 @@
 ﻿using App.Gwin.Attributes;
+using App.Gwin.Entities.MultiLanguage;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,6 +16,7 @@ namespace StockManagement
   //  [Menu(Group ="Societe")]
     public partial class FormAjouterGroupe : Form
     {
+        public List<string> ls = new List<string>();
         public FormAjouterGroupe()
         {
             InitializeComponent();
@@ -22,8 +24,86 @@ namespace StockManagement
     DAL.ModelContext db = new DAL.ModelContext();
         private void FormAjouterGroupe_Load(object sender, EventArgs e)
         {
+          
          //  List<Entities.MaterialCategory> Lm =   db.MaterialCategories.ToList<Entities.MaterialCategory>();
-            comboBoxcategorie.DataSource = db.MaterialCategories.ToList<Entities.MaterialCategory>(); ;
+            comboBoxcategorie.DataSource = db.MaterialCategories.ToList<Entities.MaterialCategory>();
+            comboBoxcategorie.DisplayMember = "Designation";
+            comboBoxcategorie.ValueMember = "id";
+            comboBoxdelivry.DataSource = db.Deliveries.ToList<Entities.Delivery>();
+            comboBoxdelivry.DisplayMember = "Market";
+            comboBoxdelivry.ValueMember = "id";
+        
+       
+
+        }
+
+        private void comboBoxcategorie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxstate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button_Ajoutr_Click(object sender, EventArgs e)
+        {
+
+            
+                    listBox1.Items.Add(serieNumberTextBox.Text);
+                    ls.Add(serieNumberTextBox.Text);
+                    serieNumberTextBox.Text = "";
+
+             
+                
+                
+            
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            for (int i=0; i < ls.Count; i++)
+            {
+                Entities.Material m = new Entities.Material();
+                m.SerieNumber = ls[i].ToString();
+
+                m.Designation= new LocalizedString();
+                // MessageBox.Show(ls[i].ToString());
+                m.Designation.Current = designationTextBox.Text ;
+                m.UpdateServiceDate = updateServiceDateDateTimePicker.Value;
+                m.Observation = new LocalizedString();
+                m.Observation.Current = observationTextBox.Text;
+                m.dimension = float.Parse(dimensionTextBox.Text);
+                m.INN_Number = iNN_NumberTextBox.Text;
+                m.MaterialCategory = db.MaterialCategories.Find(int.Parse(comboBoxcategorie.SelectedValue.ToString()));
+                m.Delivery = db.Deliveries.Find(int.Parse(comboBoxdelivry.SelectedValue.ToString()));
+                m.InventoryNumber = new LocalizedString();
+                m.InventoryNumber.Current = textBoxInventorynumber.Text;
+                m.Mark = new LocalizedString();
+                m.Mark.Current = textBoxMark.Text;
+                m.Model = new LocalizedString();
+                m.Model.Current = textBoxModel.Text;
+                    db.Materials.Add(m);
+                db.SaveChanges();
+
+            }
+            MessageBox.Show("l operation a effectuer");
+
+        }
+
+        private void buttonSupprimer_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem.ToString());
+            ls.Remove(serieNumberTextBox.Text);
+        }
+
+        private void iNN_NumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- StockManagement/StockManagement/Presentations/MaterialManger/FormAjouterGroupe.designer.cs
+++ StockManagement/StockManagement/Presentations/MaterialManger/FormAjouterGroupe.designer.cs
@@ -29,14 +29,17 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.Label serieNumberLabel;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAjouterGroupe));
             System.Windows.Forms.Label designationLabel;
             System.Windows.Forms.Label updateServiceDateLabel;
             System.Windows.Forms.Label observationLabel;
             System.Windows.Forms.Label dimensionLabel;
             System.Windows.Forms.Label iNN_NumberLabel;
-            System.Windows.Forms.Label physicalStateLabel;
             System.Windows.Forms.Label materialCategoryLabel;
             System.Windows.Forms.Label deliveryLabel;
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
             this.serieNumberTextBox = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button_Ajoutr = new System.Windows.Forms.Button();
@@ -46,206 +49,184 @@
             this.dimensionTextBox = new System.Windows.Forms.TextBox();
             this.iNN_NumberTextBox = new System.Windows.Forms.TextBox();
             this.buttonSupprimer = new System.Windows.Forms.Button();
-            this.comboBoxstate = new System.Windows.Forms.ComboBox();
-            this.comboBoxcategorie = new System.Windows.Forms.ComboBox();
             this.comboBoxdelivry = new System.Windows.Forms.ComboBox();
+            this.comboBoxcategorie = new System.Windows.Forms.ComboBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBoxInventorynumber = new System.Windows.Forms.TextBox();
+            this.textBoxMark = new System.Windows.Forms.TextBox();
+            this.textBoxModel = new System.Windows.Forms.TextBox();
             serieNumberLabel = new System.Windows.Forms.Label();
             designationLabel = new System.Windows.Forms.Label();
             updateServiceDateLabel = new System.Windows.Forms.Label();
             observationLabel = new System.Windows.Forms.Label();
             dimensionLabel = new System.Windows.Forms.Label();
             iNN_NumberLabel = new System.Windows.Forms.Label();
-            physicalStateLabel = new System.Windows.Forms.Label();
             materialCategoryLabel = new System.Windows.Forms.Label();
             deliveryLabel = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // serieNumberLabel
             // 
-            serieNumberLabel.AutoSize = true;
-            serieNumberLabel.Location = new System.Drawing.Point(462, 29);
+            resources.ApplyResources(serieNumberLabel, "serieNumberLabel");
             serieNumberLabel.Name = "serieNumberLabel";
-            serieNumberLabel.Size = new System.Drawing.Size(74, 13);
-            serieNumberLabel.TabIndex = 115;
-            serieNumberLabel.Text = "Serie Number:";
-            // 
-            // serieNumberTextBox
-            // 
-            this.serieNumberTextBox.Location = new System.Drawing.Point(561, 26);
-            this.serieNumberTextBox.Name = "serieNumberTextBox";
-            this.serieNumberTextBox.Size = new System.Drawing.Size(200, 20);
-            this.serieNumberTextBox.TabIndex = 116;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(465, 73);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(267, 368);
-            this.listBox1.TabIndex = 121;
-            // 
-            // button_Ajoutr
-            // 
-            this.button_Ajoutr.Location = new System.Drawing.Point(738, 97);
-            this.button_Ajoutr.Name = "button_Ajoutr";
-            this.button_Ajoutr.Size = new System.Drawing.Size(75, 23);
-            this.button_Ajoutr.TabIndex = 122;
-            this.button_Ajoutr.Text = "+";
-            this.button_Ajoutr.UseVisualStyleBackColor = true;
             // 
             // designationLabel
             // 
-            designationLabel.AutoSize = true;
-            designationLabel.Location = new System.Drawing.Point(12, 50);
+            resources.ApplyResources(designationLabel, "designationLabel");
             designationLabel.Name = "designationLabel";
-            designationLabel.Size = new System.Drawing.Size(66, 13);
-            designationLabel.TabIndex = 123;
-            designationLabel.Text = "Designation:";
-            // 
-            // designationTextBox
-            // 
-            this.designationTextBox.Location = new System.Drawing.Point(168, 50);
-            this.designationTextBox.Name = "designationTextBox";
-            this.designationTextBox.Size = new System.Drawing.Size(210, 20);
-            this.designationTextBox.TabIndex = 124;
             // 
             // updateServiceDateLabel
             // 
-            updateServiceDateLabel.AutoSize = true;
-            updateServiceDateLabel.Location = new System.Drawing.Point(12, 96);
+            resources.ApplyResources(updateServiceDateLabel, "updateServiceDateLabel");
             updateServiceDateLabel.Name = "updateServiceDateLabel";
-            updateServiceDateLabel.Size = new System.Drawing.Size(110, 13);
-            updateServiceDateLabel.TabIndex = 125;
-            updateServiceDateLabel.Text = "Update Service Date:";
-            // 
-            // updateServiceDateDateTimePicker
-            // 
-            this.updateServiceDateDateTimePicker.Location = new System.Drawing.Point(168, 96);
-            this.updateServiceDateDateTimePicker.Name = "updateServiceDateDateTimePicker";
-            this.updateServiceDateDateTimePicker.Size = new System.Drawing.Size(210, 20);
-            this.updateServiceDateDateTimePicker.TabIndex = 126;
             // 
             // observationLabel
             // 
-            observationLabel.AutoSize = true;
-            observationLabel.Location = new System.Drawing.Point(12, 129);
+            resources.ApplyResources(observationLabel, "observationLabel");
             observationLabel.Name = "observationLabel";
-            observationLabel.Size = new System.Drawing.Size(67, 13);
-            observationLabel.TabIndex = 126;
-            observationLabel.Text = "Observation:";
-            // 
-            // observationTextBox
-            // 
-            this.observationTextBox.Location = new System.Drawing.Point(168, 126);
-            this.observationTextBox.Name = "observationTextBox";
-            this.observationTextBox.Size = new System.Drawing.Size(210, 20);
-            this.observationTextBox.TabIndex = 127;
             // 
             // dimensionLabel
             // 
-            dimensionLabel.AutoSize = true;
-            dimensionLabel.Location = new System.Drawing.Point(12, 163);
+            resources.ApplyResources(dimensionLabel, "dimensionLabel");
             dimensionLabel.Name = "dimensionLabel";
-            dimensionLabel.Size = new System.Drawing.Size(57, 13);
-            dimensionLabel.TabIndex = 127;
-            dimensionLabel.Text = "dimension:";
-            // 
-            // dimensionTextBox
-            // 
-            this.dimensionTextBox.Location = new System.Drawing.Point(168, 156);
-            this.dimensionTextBox.Name = "dimensionTextBox";
-            this.dimensionTextBox.Size = new System.Drawing.Size(210, 20);
-            this.dimensionTextBox.TabIndex = 128;
             // 
             // iNN_NumberLabel
             // 
-            iNN_NumberLabel.AutoSize = true;
-            iNN_NumberLabel.Location = new System.Drawing.Point(12, 202);
+            resources.ApplyResources(iNN_NumberLabel, "iNN_NumberLabel");
             iNN_NumberLabel.Name = "iNN_NumberLabel";
-            iNN_NumberLabel.Size = new System.Drawing.Size(69, 13);
-            iNN_NumberLabel.TabIndex = 128;
-            iNN_NumberLabel.Text = "INN Number:";
-            // 
-            // iNN_NumberTextBox
-            // 
-            this.iNN_NumberTextBox.Location = new System.Drawing.Point(168, 195);
-            this.iNN_NumberTextBox.Name = "iNN_NumberTextBox";
-            this.iNN_NumberTextBox.Size = new System.Drawing.Size(210, 20);
-            this.iNN_NumberTextBox.TabIndex = 129;
-            // 
-            // physicalStateLabel
-            // 
-            physicalStateLabel.AutoSize = true;
-            physicalStateLabel.Location = new System.Drawing.Point(12, 234);
-            physicalStateLabel.Name = "physicalStateLabel";
-            physicalStateLabel.Size = new System.Drawing.Size(77, 13);
-            physicalStateLabel.TabIndex = 129;
-            physicalStateLabel.Text = "Physical State:";
             // 
             // materialCategoryLabel
             // 
-            materialCategoryLabel.AutoSize = true;
-            materialCategoryLabel.Location = new System.Drawing.Point(12, 268);
+            resources.ApplyResources(materialCategoryLabel, "materialCategoryLabel");
             materialCategoryLabel.Name = "materialCategoryLabel";
-            materialCategoryLabel.Size = new System.Drawing.Size(92, 13);
-            materialCategoryLabel.TabIndex = 130;
-            materialCategoryLabel.Text = "Material Category:";
             // 
             // deliveryLabel
             // 
-            deliveryLabel.AutoSize = true;
-            deliveryLabel.Location = new System.Drawing.Point(12, 302);
+            resources.ApplyResources(deliveryLabel, "deliveryLabel");
             deliveryLabel.Name = "deliveryLabel";
-            deliveryLabel.Size = new System.Drawing.Size(48, 13);
-            deliveryLabel.TabIndex = 131;
-            deliveryLabel.Text = "Delivery:";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.Name = "label3";
+            // 
+            // serieNumberTextBox
+            // 
+            resources.ApplyResources(this.serieNumberTextBox, "serieNumberTextBox");
+            this.serieNumberTextBox.Name = "serieNumberTextBox";
+            // 
+            // listBox1
+            // 
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Name = "listBox1";
+            // 
+            // button_Ajoutr
+            // 
+            resources.ApplyResources(this.button_Ajoutr, "button_Ajoutr");
+            this.button_Ajoutr.Name = "button_Ajoutr";
+            this.button_Ajoutr.UseVisualStyleBackColor = true;
+            this.button_Ajoutr.Click += new System.EventHandler(this.button_Ajoutr_Click);
+            // 
+            // designationTextBox
+            // 
+            resources.ApplyResources(this.designationTextBox, "designationTextBox");
+            this.designationTextBox.Name = "designationTextBox";
+            // 
+            // updateServiceDateDateTimePicker
+            // 
+            resources.ApplyResources(this.updateServiceDateDateTimePicker, "updateServiceDateDateTimePicker");
+            this.updateServiceDateDateTimePicker.Name = "updateServiceDateDateTimePicker";
+            // 
+            // observationTextBox
+            // 
+            resources.ApplyResources(this.observationTextBox, "observationTextBox");
+            this.observationTextBox.Name = "observationTextBox";
+            // 
+            // dimensionTextBox
+            // 
+            resources.ApplyResources(this.dimensionTextBox, "dimensionTextBox");
+            this.dimensionTextBox.Name = "dimensionTextBox";
+            // 
+            // iNN_NumberTextBox
+            // 
+            resources.ApplyResources(this.iNN_NumberTextBox, "iNN_NumberTextBox");
+            this.iNN_NumberTextBox.Name = "iNN_NumberTextBox";
+            this.iNN_NumberTextBox.TextChanged += new System.EventHandler(this.iNN_NumberTextBox_TextChanged);
             // 
             // buttonSupprimer
             // 
-            this.buttonSupprimer.Location = new System.Drawing.Point(738, 158);
+            resources.ApplyResources(this.buttonSupprimer, "buttonSupprimer");
             this.buttonSupprimer.Name = "buttonSupprimer";
-            this.buttonSupprimer.Size = new System.Drawing.Size(75, 23);
-            this.buttonSupprimer.TabIndex = 133;
-            this.buttonSupprimer.Text = "-";
             this.buttonSupprimer.UseVisualStyleBackColor = true;
-            // 
-            // comboBoxstate
-            // 
-            this.comboBoxstate.FormattingEnabled = true;
-            this.comboBoxstate.Location = new System.Drawing.Point(168, 231);
-            this.comboBoxstate.Name = "comboBoxstate";
-            this.comboBoxstate.Size = new System.Drawing.Size(210, 21);
-            this.comboBoxstate.TabIndex = 134;
-            // 
-            // comboBoxcategorie
-            // 
-            this.comboBoxcategorie.FormattingEnabled = true;
-            this.comboBoxcategorie.Location = new System.Drawing.Point(168, 260);
-            this.comboBoxcategorie.Name = "comboBoxcategorie";
-            this.comboBoxcategorie.Size = new System.Drawing.Size(210, 21);
-            this.comboBoxcategorie.TabIndex = 135;
+            this.buttonSupprimer.Click += new System.EventHandler(this.buttonSupprimer_Click);
             // 
             // comboBoxdelivry
             // 
+            resources.ApplyResources(this.comboBoxdelivry, "comboBoxdelivry");
             this.comboBoxdelivry.FormattingEnabled = true;
-            this.comboBoxdelivry.Location = new System.Drawing.Point(168, 299);
             this.comboBoxdelivry.Name = "comboBoxdelivry";
-            this.comboBoxdelivry.Size = new System.Drawing.Size(210, 21);
-            this.comboBoxdelivry.TabIndex = 136;
+            // 
+            // comboBoxcategorie
+            // 
+            resources.ApplyResources(this.comboBoxcategorie, "comboBoxcategorie");
+            this.comboBoxcategorie.DisplayMember = "Id";
+            this.comboBoxcategorie.FormattingEnabled = true;
+            this.comboBoxcategorie.Name = "comboBoxcategorie";
+            this.comboBoxcategorie.ValueMember = "Id";
+            this.comboBoxcategorie.SelectedIndexChanged += new System.EventHandler(this.comboBoxcategorie_SelectedIndexChanged);
+            // 
+            // buttonSave
+            // 
+            resources.ApplyResources(this.buttonSave, "buttonSave");
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBoxInventorynumber
+            // 
+            resources.ApplyResources(this.textBoxInventorynumber, "textBoxInventorynumber");
+            this.textBoxInventorynumber.Name = "textBoxInventorynumber";
+            // 
+            // textBoxMark
+            // 
+            resources.ApplyResources(this.textBoxMark, "textBoxMark");
+            this.textBoxMark.Name = "textBoxMark";
+            // 
+            // textBoxModel
+            // 
+            resources.ApplyResources(this.textBoxModel, "textBoxModel");
+            this.textBoxModel.Name = "textBoxModel";
             // 
             // FormAjouterGroupe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(833, 741);
+            this.Controls.Add(label3);
+            this.Controls.Add(this.textBoxModel);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.textBoxMark);
+            this.Controls.Add(this.textBoxInventorynumber);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.comboBoxdelivry);
             this.Controls.Add(this.comboBoxcategorie);
-            this.Controls.Add(this.comboBoxstate);
             this.Controls.Add(this.buttonSupprimer);
             this.Controls.Add(deliveryLabel);
             this.Controls.Add(materialCategoryLabel);
-            this.Controls.Add(physicalStateLabel);
             this.Controls.Add(iNN_NumberLabel);
             this.Controls.Add(this.iNN_NumberTextBox);
             this.Controls.Add(dimensionLabel);
@@ -261,7 +242,6 @@
             this.Controls.Add(serieNumberLabel);
             this.Controls.Add(this.serieNumberTextBox);
             this.Name = "FormAjouterGroupe";
-            this.Text = "FormAjouterGroupe";
             this.Load += new System.EventHandler(this.FormAjouterGroupe_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -278,8 +258,11 @@
         private System.Windows.Forms.TextBox dimensionTextBox;
         private System.Windows.Forms.TextBox iNN_NumberTextBox;
         private System.Windows.Forms.Button buttonSupprimer;
-        private System.Windows.Forms.ComboBox comboBoxstate;
-        private System.Windows.Forms.ComboBox comboBoxcategorie;
         private System.Windows.Forms.ComboBox comboBoxdelivry;
+        private System.Windows.Forms.ComboBox comboBoxcategorie;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBoxInventorynumber;
+        private System.Windows.Forms.TextBox textBoxMark;
+        private System.Windows.Forms.TextBox textBoxModel;
     }
 }
